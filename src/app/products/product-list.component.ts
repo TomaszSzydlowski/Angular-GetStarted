7f014ad332bfa713aeca556ad0021c8bc0e42dd1
TomaszSzydlowski <TSzydlowski@protonmail.com>
2020-05-29 14:53:16 +0200

    Filtering a list

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,15 +4,32 @@ import { IProduct } from './products';
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
-  styleUrls:['./product-list.component.css']
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
+
 
   imageWidth: number = 50;
   imageMargin: number = 2;
   pageTitle: string = 'Product List';
   showImage: boolean = false;
-  listFilter:string='cart';
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  filteredProducts: IProduct[];
   products: IProduct[] = [
     {
       productId: 1,
@@ -45,6 +62,11 @@ export class ProductListComponent implements OnInit{
       imageUrl: 'assets/images/hammer.png',
     },
   ];
+
+  constructor(){
+    this.filteredProducts=this.products;
+    this.listFilter='cart';
+  }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
